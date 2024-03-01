1d771efcb15898656a11bd1124b91aa9da7e67c3
marcos oliveira <marcos.rogerio.oliveira23@gmail.com>
2024-03-01 16:47:57 -0300

    first commit

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_pizza = Restaurante('Pizza Express', 'Pizza')
-#restaurante_churrasco = Restaurante('Churrasco no Ponto', 'Carne')
-#restaurante_hamburguer = Restaurante('Big Kahuna Burguer', 'Hamburgueria')
-
-restaurante_pizza.receber_avaliacao('Marcos', 10)
-restaurante_pizza.receber_avaliacao('maria', 8)
-restaurante_pizza.receber_avaliacao('will', 5)
-
+bebida_suco = Bebida('Suco de Laranja', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
+prato_pizza = Prato('Pizza de Calabresa', 12.0, 'calabreasa com queijo')
+prato_pizza.aplicar_desconto()
+restaurante_pizza.adicionar_no_cardapio(bebida_suco)
+restaurante_pizza.adicionar_no_cardapio(prato_pizza)
 
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_pizza.exibir_cardapio
 
 if __name__ == '__main__':
     main()
